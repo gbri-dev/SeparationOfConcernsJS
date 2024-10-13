4cb3a0a7fe12bb9941bedbf1842334ac95f45b78
Gbri-dev <gabriel.camposdasilva@hotmail.com>
2024-10-13 17:02:10 -0300

    feat(usando o padrão): observer

--- factory.js
+++ factory.js
@@ -6,24 +6,39 @@
 //     let object = {}
 //     return object
 // }
-
 // const result = createObject()
-
 // console.log("Node js!!! \n" + result)
 
-function createPerson(nome, sobrenome) {
-    let pessoa = {}
-    pessoa.nome = nome
-    pessoa.sobrenome = sobrenome
+// Segundo exemplo
+// function createPerson(nome, sobrenome) {
+//     let pessoa = {
+//         nome: nome,
+//         sobrenome: sobrenome,
+//         nomeCompleto: function() {
+//             return `${pessoa.nome} ${pessoa.sobrenome}`
+//         },
+//     } 
+//     return pessoa
+// }
 
-    function nomeCompleto() {
-        return `${pessoa.nome} ${pessoa.sobrenome}`
+// let pessoa1 = createPerson("João", "de Patmos")
+// let pessoa2 = createPerson("São", "Bartolomeu")
+// console.log(pessoa2)
+// console.log(pessoa1["nomeCompleto"])
+// const nomeCompletoPs1 = pessoa1["nomeCompleto"]
+// console.log(nomeCompletoPs1())
+
+// Exemplo de calculadora
+
+const calculadora = {
+    somar: function(valor1, valor2) {
+        return valor1 + valor2
+    },
+    multiplicar: function(valor1, valor2) {
+        return valor1 * valor2
     }
-    pessoa.nomeCompleto = nomeCompleto
-    return pessoa
 }
 
-let pessoa1 = createPerson("João", "de Patmos")
-let pessoa2 = createPerson("São", "Bartolomeu")
-console.log(pessoa2)
-console.log(pessoa1.nomeCompleto())
+const multiplicacao = calculadora["multiplicar"]
+
+console.log(multiplicacao(2, 2))
